d123f28162a8df57b97a3d13e099a857d2cc4cfd
anish877 <62011810+anish877@users.noreply.github.com>
2024-12-30 09:00:14 +0530

    intial

--- js/script.js
+++ js/script.js
@@ -111,7 +111,7 @@ async function displayAlbums() {
     Array.from(document.getElementsByClassName("card")).forEach(e => {
         e.addEventListener("click", async item => {
             console.log("Fetching Songs");
-            songs = await getSongs(`./songs/${item.currentTarget.dataset.folder}`);
+            songs = await getSongs(`clone/songs/${item.currentTarget.dataset.folder}`);
             playMusic(songs[0]);
         });
     });
